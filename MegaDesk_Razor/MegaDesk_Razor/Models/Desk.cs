71c7046ff370902123a648bf643bf6672bb5a368
Brad <bradkellogg7@gmail.com>
2019-03-06 20:26:06 -0700

    moved models location and added Id to desk class

---
+++ MegaDesk_Razor/MegaDesk_Razor/Models/Desk.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MegaDesk_Razor.Models
+{
+    public class Desk
+    {
+        public int ID { get; set; }
+    }
+}
