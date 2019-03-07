6153f3f05c88d182d59e87835f4036e53b74c065
waabbajak <waabbajak@gmail.com>
2019-03-06 21:10:29 -0700

    Update DeskQuote.cs

--- MegaDesk_Razor/MegaDesk_Razor/Models/DeskQuote.cs
+++ MegaDesk_Razor/MegaDesk_Razor/Models/DeskQuote.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +10,10 @@ namespace MegaDesk_Razor.Models
     {
         public int ID { get; set; }
         private string CustomerName { get; set; }
-        private DateTime QuoteDate = DateTime.Today;
+
+        [DataType(DataType.Date)]
+        private DateTime OrderDate { get; set; }
+
         private Desk newDesk = new Desk();
         private int RushDays { get; set; }
         private int QuoteAmount { get; set; }
