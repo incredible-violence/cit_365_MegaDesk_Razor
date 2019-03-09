ec8c5f286f34e6b022d98fd3e4416a9601cf56a6
waabbajak <waabbajak@gmail.com>
2019-03-08 22:10:47 -0700

    fixed fatal error

--- MegaDesk_Razor/MegaDesk_Razor/Pages/DeskQuotes/Create.cshtml.cs
+++ MegaDesk_Razor/MegaDesk_Razor/Pages/DeskQuotes/Create.cshtml.cs
@@ -23,11 +23,12 @@ namespace MegaDesk_Razor.Pages.DeskQuotes
             return Page();
         }
 
+        /*
         public async Task OnGetAsync()
         {
             // logic for building quote price based on other text input fields
         }
-
+        */
         [BindProperty]
         public DeskQuote DeskQuote { get; set; }
 
